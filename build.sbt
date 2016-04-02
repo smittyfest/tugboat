ede0bf5be9730229523b66c0cbe9205436247a1f
Eric Smith <esmith@thebrighttag.com>
2016-04-02 12:07:16 -0500

    created SBT project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "tugboat"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/org/smittyfest/tugboat/resource/TugBoatResource.scala
@@ -0,0 +1,9 @@
+package org.smittyfest.tugboat.resource
+
+/**
+  * @author smittyfest
+  * @since 4/2/16
+  */
+object TugBoatResource {
+
+}
