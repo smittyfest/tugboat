0d9966453f4e8985620a48deec413641dbf344d7
Eric Smith <esmith@thebrighttag.com>
2016-04-03 11:02:13 -0500

    Up and running

--- src/main/scala/org/smittyfest/tugboat/resource/TugBoatResource.scala
+++ src/main/scala/org/smittyfest/tugboat/resource/TugBoatResource.scala
@@ -1,6 +1,12 @@
 package org.smittyfest.tugboat.resource
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
+import org.smittyfest.tugboat.service.GreetingServiceActor
+import spray.can.Http
+import scala.concurrent.duration._
 
 /**
   * @author smittyfest
@@ -8,4 +14,7 @@ import akka.actor.ActorSystem
   */
 object TugBoatResource extends App {
   implicit val actorSystem = ActorSystem("on-spray-can")
+  val greetingActor = actorSystem.actorOf(Props[GreetingServiceActor], "greeting-service")
+  implicit val timeout = Timeout(5.seconds)
+  IO(Http) ? Http.Bind(greetingActor, interface = "localhost", port = 4567)
 }
--- src/main/scala/org/smittyfest/tugboat/service/GreetingService.scala
+++ src/main/scala/org/smittyfest/tugboat/service/GreetingService.scala
@@ -1,9 +1,30 @@
 package org.smittyfest.tugboat.service
 
+import akka.actor.Actor
+import spray.http.MediaTypes.`text/html`
+import spray.routing.HttpService
+
 /**
   * @author smittyfest
   * @since 4/2/16
   */
-trait GreetingService {
-
+class GreetingServiceActor extends Actor with GreetingService {
+  def actorRefFactory = context
+  def receive = runRoute(greetingRoute)
+}
+trait GreetingService extends HttpService {
+  val greetingRoute =
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
 }
